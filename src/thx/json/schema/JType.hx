2d704576de24dda4dd05c07a21ce7e2e801adcf8
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-01-20 10:04:57 -0700

    Add JType

---
+++ src/thx/json/schema/JType.hx
@@ -0,0 +1,35 @@
+package thx.json.schema;
+
+enum JTypeADT {
+  JArrayT;
+  JBoolT;
+  JIntT;
+  JNumT;
+  JNullT;
+  JObjT;
+  JStringT;
+}
+
+abstract JType (JTypeADT) from JTypeADT to JTypeADT {
+  public static function forValue(v: JValue): JType
+    return switch v {
+      case JString(_): JStringT;
+      case JNum(_): JNumT;
+      case JBool(_): JBoolT;
+      case JArray(_): JArrayT;
+      case JObject(_): JObjT;
+      case JNull: JNullT;
+    };
+
+  public function name(): String
+    return switch this {
+      case JArrayT:  "array";
+      case JBoolT:   "boolean";
+      case JIntT:    "integer";
+      case JNumT:    "number";
+      case JNullT:   "null";
+      case JObjT:    "object";
+      case JStringT: "string";
+    };
+}
+
