1712e132686fbe4ab68a99bd78a707228d73d84d
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-01-21 11:31:08 -0700

    Added path-at-failure information for JSearch syntax.

--- src/thx/json/JValue.hx
+++ src/thx/json/JValue.hx
@@ -1,8 +1,11 @@
 package thx.json;
 
 import haxe.ds.Option;
+import thx.Either;
+import thx.json.schema.JType;
 
 using thx.Arrays;
+using thx.Eithers;
 using thx.Functions;
 using thx.Maps;
 using thx.Options;
@@ -21,6 +24,18 @@ enum JValueADT {
   JNull;
 }
 
+enum JPath {
+  Property(name: String, tail: JPath);
+  Index(idx: Int, tail: JPath);
+  Empty;
+}
+
+enum JPathError {
+  NoSuchProperty(name: String, object: JValue);
+  IndexOutOfRange(idx: Int, array: JValue);
+  TypeMismatch(expected: JType, found: JValue);
+}
+
 abstract JValue (JValueADT) from JValueADT to JValueADT {
   inline public static function jString(s: String): JValue return JString(s);
   inline public static function jNum(x: Float): JValue return JNum(x);
@@ -36,25 +51,53 @@ abstract JValue (JValueADT) from JValueADT to JValueADT {
     return JObject(m.tuples().map(function(t) return { name: t._0, value: t._1 }));
 
   @:op(A / B)
-  public function getProperty(name: String): JSearch return switch this {
-    case JObject(xs): xs.findOption(function(a) return a.name == name).map(function(a) return a.value);
-    case _: None;
+  public function getProperty(name: String): JSearch return {
+    path : Property(name, Empty),
+    value: switch this {
+      case JObject(xs): 
+        switch xs.findOption(function(a) return a.name == name) {
+          case Some(a): Right(a.value);
+          case None:    Left(NoSuchProperty(name, this));
+        };
+
+      case other: 
+        Left(TypeMismatch(JObjT, other));
+    }
   };
 
   @:arrayAccess
-  public function getIndex(idx: Int): JSearch return switch this {
-    case JArray(xs): if (xs.length > idx) Some(xs[idx]) else None;
-    case _: None;
+  public function getIndex(idx: Int): JSearch return {
+    path: Index(idx, Empty),
+    value: switch this {
+      case JArray(xs): 
+        if (idx >= 0 && xs.length > idx) Right(xs[idx]) else Left(IndexOutOfRange(idx, this));
+      case other: 
+        Left(TypeMismatch(JArrayT, other));
+    }
   };
 }
 
-abstract JSearch (Option<JValue>) from Option<JValue> to Option<JValue> {
+abstract JSearch ({ path: JPath, value: Either<JPathError, JValue> }) from { path: JPath, value: Either<JPathError, JValue> } {
+  inline public function repr(): { path: JPath, value: Either<JPathError, JValue> } return this;
+
   @:op(A / B)
-  public function getProperty(name: String): JSearch 
-    return this.flatMap(function(jv) return jv / name);
+  public function getProperty(name: String): JSearch return switch this.value {
+    case Left(error): this;
+    case Right(jv):   { path: Property(name, this.path), value: (jv / name).repr().value };
+  };
 
   @:arrayAccess
-  public function getIndex(idx: Int): JSearch 
-    return this.flatMap(function(jv) return jv[idx]);
+  public function getIndex(idx: Int): JSearch return switch this.value {
+    case Left(error): this;
+    case Right(jv):   { path: Index(idx, this.path), value: jv[idx].repr().value };
+  };
+
+  @:to
+  public function run(): Either<JSearchError, JValue> 
+    return this.value.leftMap(function(e) return { path: this.path, error: e });
 }
 
+typedef JSearchError = {
+  path: JPath,
+  error: JPathError
+};
--- test/thx/json/TestJValue.hx
+++ test/thx/json/TestJValue.hx
@@ -1,22 +1,39 @@
 package thx.json;
 
 import haxe.ds.Option;
+import thx.Either;
+
 import utest.Assert;
+
+import thx.json.JValue;
 import thx.json.JValue.*;
 import thx.json.JValue.JSearch;
+import thx.json.schema.JType;
 
 class TestJValue {
+  static var ox = jObject([ "x" => jNum(3) ]);
+  static var arr = jArray([ox, jNum(4)]);
+  static var obj = jObject([
+    "a" => jNum(1), 
+    "b" => jNum(2), 
+    "c" => arr,
+    "n" => jNull
+  ]);
+
   public function new() { }
 
   public function testPathSyntax() {
-    var obj = jObject([
-      "a" => jNum(1), 
-      "b" => jNum(2), 
-      "c" => jArray([jNum(3), jNum(4)]),
-      "n" => jNull
-    ]);
+    Assert.same(Right(jNum(4)), (obj / "c")[1].run());
+  }
 
-    Assert.same(Some(jNum(4)), (obj / "c")[1]);
+  public function testArrayError() {
+    Assert.same(Left({ path: Index(2, Property("c", Empty)), error: IndexOutOfRange(2, arr) }), (obj / "c")[2].run());
+    Assert.same(Left({ path: Index(2, Property("n", Empty)), error: TypeMismatch(JArrayT, jNull) }), (obj / "n")[2].run());
+    Assert.same(Left({ path: Index(2, Property("c", Empty)), error: IndexOutOfRange(2, arr) }), ((obj / "c")[2] / "x").run());
+  }
+
+  public function testPropertyError() {
+    Assert.same(Left({ path: Property("y", Index(0, Property("c", Empty))), error: NoSuchProperty("y", ox) }), ((obj / "c")[0] / "y").run());
   }
 }
 
